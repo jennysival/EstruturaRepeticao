5f8010d5d0bae542f95d7db3b33a9dffe368729a
jennysival <jeniffersival@gmail.com>
2022-03-31 11:56:25 -0300

    atualizar exercicio 1 exibindo os numeros pares até 100

---
+++ src/Exercicio1NumerosPares.java
@@ -0,0 +1,9 @@
+public class Exercicio1NumerosPares {
+    public static void main(String[] args) {
+        int i;
+        for(i = 1; i < 100; i++){
+            if(i % 2 == 0){
+            System.out.println(i);
+        }
+        }
+    }}
